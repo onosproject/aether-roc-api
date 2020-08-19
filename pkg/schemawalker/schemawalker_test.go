73d4df708c46c5aa44172a01c3a5e1dc14ee4c41
Sean Condon <sean@opennetworking.org>
2020-08-19 09:01:14 +0100

    Initial commit
    Includes an **experimental** tool to generate Swagger.yaml from modelplugin

---
+++ pkg/schemawalker/schemawalker_test.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+//
+
+package schemawalker
+
+import "testing"
+
+func TestExtractPaths(t *testing.T) {
+
+}
