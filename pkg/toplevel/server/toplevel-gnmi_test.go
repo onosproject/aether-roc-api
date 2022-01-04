c5f2d0f9ffadf6946399c92a366079fc0a5a8a1a
Matteo Scandolo <matteo.scandolo@gmail.com>
2022-01-04 15:55:17 -0800

    [AETHER-2651] Throwing an error if invalid fields are sent to PatchAetherRocApi endpoint

---
+++ pkg/toplevel/server/toplevel-gnmi_test.go
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package server
+
+import (
+	"context"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestGnmiPachAetherRocApi_wrongFormat(t *testing.T) {
+
+	server := &ServerImpl{
+		GnmiClient:    nil,
+		Authorization: false,
+	}
+
+	body := []byte(`{"foo":"bar"}`)
+	_, err := server.gnmiPatchAetherRocAPI(context.Background(), body, "")
+	assert.Error(t, err, `unable to unmarshal JSON as types.PatchBody: json: unknown field "foo"`)
+}
+
+func TestGnmiPachAetherRocApi_wrongFormat2(t *testing.T) {
+
+	server := &ServerImpl{
+		GnmiClient:    nil,
+		Authorization: false,
+	}
+
+	body := []byte(`{"Updates":{}}`)
+	_, err := server.gnmiPatchAetherRocAPI(context.Background(), body, "")
+	assert.Error(t, err, `unable to convert types.PatchBody to gNMI default-target cannot be blank`)
+}
